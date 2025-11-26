57e3d47a498177f5f4f276ac1a5fa91c0f411ad0
Clarissa <clarissamorita1@gmail.com>
2025-11-26 10:53:13 -0300

    conexao e banco

---
+++ CamadaDados/DCategoria.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+//using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+namespace CamadaDados
+{
+    internal class DCategoria
+    {
+        private int _Idcategoria;
+        private string _Nome;
+        private string _Descricao;
+        private string _Textobuscar;
+
+        public int Idcategoria { get => _Idcategoria; set => _Idcategoria = value; }
+        public string Nome { get => _Nome; set => _Nome = value; }
+        public string Descricao { get => _Descricao; set => _Descricao = value; }
+        public string Textobuscar { get => _Textobuscar; set => _Textobuscar = value; }
+
+        public DCategoria() { }
+
+        public string Dcategoria(int idcategoria, string nome, string descricao, string textobuscar)
+        {
+            this.Idcategoria = idcategoria;
+            this.Nome = nome;
+            this.Descricao = descricao;
+            this.Textobuscar = textobuscar;
+            return "true";
+        }
+
+        //Metodo Inserir
+        public string Inserir(DCategoria categoria)
+        {
+            string resp = "";
+            SqlConnection SqlCon = new SqlConnection();
+
+            try
+            {
+                SqlCon.ConnectionString = Conexao.Cn;
+                SqlCon.Open();
+
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "SP_INSERIR_CATEGORIA";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdcategoria = new SqlParameter();
+                ParIdcategoria.ParameterName = "@idcategoria";
+                ParIdcategoria.SqlDbType = SqlDbType.Int;
+                ParIdcategoria.Direction = ParameterDirection.Output;
+                SqlCmd.Parameters.Add(ParIdcategoria);
+
+                SqlParameter ParNome = new SqlParameter();
+                ParNome.ParameterName = "@nome";
+                ParNome.SqlDbType = SqlDbType.VarChar;
+                ParNome.Size = 50;
+                ParNome.Value = categoria.Nome;
+                SqlCmd.Parameters.Add(ParNome);
+
+                SqlParameter ParDescricao = new SqlParameter();
+                ParDescricao.ParameterName = "@descricao"; 
+                ParDescricao.SqlDbType = SqlDbType.VarChar; 
+                ParDescricao.Size = 256; 
+                ParDescricao.Value = categoria.Descricao;
+                SqlCmd.Parameters.Add(ParDescricao);
+
+                resp = SqlCmd.ExecuteNonQuery() == 1 ? "OK" : "Não foi inserido!";
+            }
+            catch (Exception err)
+            {
+                resp = err.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+
+            return resp; 
+        }
+
+        //Metodo Editar
+        public string Editar(DCategoria categoria)
+        {
+            string resp = "";
+            SqlConnection SqlCon = new SqlConnection();
+
+            try
+            {
+                SqlCon.ConnectionString = Conexao.Cn;
+                SqlCon.Open();
+
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "SP_EDITAR_CATEGORIA";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdcategoria = new SqlParameter();
+                ParIdcategoria.ParameterName = "@idcategoria";
+                ParIdcategoria.SqlDbType = SqlDbType.Int;
+                ParIdcategoria.Value = categoria.Idcategoria;
+                SqlCmd.Parameters.Add(ParIdcategoria);
+
+                SqlParameter ParNome = new SqlParameter();
+                ParNome.ParameterName = "@nome";
+                ParNome.SqlDbType = SqlDbType.VarChar;
+                ParNome.Size = 50;
+                ParNome.Value = categoria.Nome;
+                SqlCmd.Parameters.Add(ParNome);
+
+                SqlParameter ParDescricao = new SqlParameter();
+                ParDescricao.ParameterName = "@descricao";  
+                ParDescricao.SqlDbType = SqlDbType.VarChar;  
+                ParDescricao.Size = 256; 
+                ParDescricao.Value = categoria.Descricao;
+                SqlCmd.Parameters.Add(ParDescricao);
+
+                resp = SqlCmd.ExecuteNonQuery() == 1 ? "OK" : "Não foi editado!";
+            }
+            catch (Exception err)
+            {
+                resp = err.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+
+            return resp;  
+        }
+
+        //Metodo Excluir
+        public string Excluir(DCategoria categoria)
+        {
+            string resp = "";
+            SqlConnection SqlCon = new SqlConnection();
+
+            try
+            {
+                SqlCon.ConnectionString = Conexao.Cn;
+                SqlCon.Open();
+
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "SP_DELETE_CATEGORIA";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdcategoria = new SqlParameter();
+                ParIdcategoria.ParameterName = "@idcategoria";
+                ParIdcategoria.SqlDbType = SqlDbType.Int;
+                ParIdcategoria.Value = categoria.Idcategoria;
+                SqlCmd.Parameters.Add(ParIdcategoria);
+
+                resp = SqlCmd.ExecuteNonQuery() == 1 ? "OK" : "Não foi excluída!";
+            }
+            catch (Exception err)
+            {
+                resp = err.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+
+            return resp;  
+        }
+
+        //Metodo Mostrar
+        public DataTable Mostrar()  
+        {
+            DataTable DtResultado = new DataTable("categoria"); //tabela de consulta
+            SqlConnection SqlCon = new SqlConnection();
+
+            try
+            {
+                SqlCon.ConnectionString = Conexao.Cn;
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "SP_MOSTAR_CATEGORIA";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd); //faz consulta, armazena e exibe
+                SqlDat.Fill(DtResultado);
+
+            }
+            catch (Exception)
+            {
+               
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+
+            return DtResultado;
+        }
+
+        //Metodo BuscarNome
+        public DataTable BuscarNome(DCategoria categoria)
+        {
+            DataTable DtResultado = new DataTable("categoria");
+            SqlConnection SqlCon = new SqlConnection();
+
+            try
+            {
+                SqlCon.ConnectionString = Conexao.Cn;
+                SqlCon.Open();
+
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "SP_BUSCAR_NOME";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParTextobuscar = new SqlParameter();
+                ParTextobuscar.ParameterName = "@textobuscar";
+                ParTextobuscar.SqlDbType = SqlDbType.VarChar;
+                ParTextobuscar.Size = 50;
+                ParTextobuscar.Value = categoria.Textobuscar;
+                SqlCmd.Parameters.Add(ParTextobuscar);
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+            }
+            catch (Exception err)
+            {
+                DtResultado = null;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+
+            return DtResultado;
+        }
+    }
+}
